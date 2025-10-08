0bb856b369f533412a157bb306f2cadeebf5593d
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-08 23:28:14 +0100

    No code changes detected.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { QrCodeModule } from './QrCodeValidator/QrCode.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [QrCodeModule],
 })
 export class AppModule {}
