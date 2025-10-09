9c39c37215695602daffc315f9da337dcf2ebe67
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-09 12:11:13 +0100

    refactor pdf method to await barcode detection and format validation

--- backend/src/QrCodeValidator/QrCode.service.ts
+++ backend/src/QrCodeValidator/QrCode.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { pdfToImage } from "./utils/pdftoimage";
 import { detectBarCodes } from "./utils/detectbarcodes";
-
+import { formatValidationAndCRUD } from "./utils/format_validation_and_crud";
 @Injectable()
 export class QrCodeService {
     async pdf(buffer: any) {
         const images = await pdfToImage(buffer);
-        detectBarCodes(images);
+        const QrCodes: any = await detectBarCodes(images);
+        formatValidationAndCRUD(QrCodes);
+// {"batch":"B20245685","category":"LOG","location":"DEP-04","serial":"SN-890879","tag_id":"TAG-00022"}
         return {message: "halloooooo"};
     }
 }
