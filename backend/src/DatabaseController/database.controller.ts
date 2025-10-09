b6a336c95deee180761d39d22f9395326b74bea9
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-09 22:24:16 +0100

    create an endpoint for fetching datat from database

--- backend/src/DatabaseController/database.controller.ts
+++ backend/src/DatabaseController/database.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Param } from "@nestjs/common";
+import { DatabaseService } from "./database.service";
+
 
 @Controller('api')
 export class DatabaseController {
+    constructor(private readonly databaseservice: DatabaseService) {}
 
-    @Get("get-uploadId")
-    async getDataByUploadId(@Param() uploadId: string) {
-        return "hello wo;rld";
+    @Get("get-data/:uploadId")
+    async getDataByUploadId(@Param('uploadId') uploadId: string) {
+
+        return this.databaseservice.getDataById(uploadId);
     }
 }
