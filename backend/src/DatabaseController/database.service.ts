032be6bfb313945735be12492c26c3f4e74fc105
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-09 22:24:51 +0100

    return an obj that contain only necessery values to the front

--- backend/src/DatabaseController/database.service.ts
+++ backend/src/DatabaseController/database.service.ts
@@ -1,6 +1,45 @@
 import { Injectable } from "@nestjs/common";
+import { MongoClient } from "mongodb";
+import { ClassCheck } from "src/QrCodeValidator/utils/interfaces/interfaces";
+
+// Connection URL
+const uri = "mongodb://mongo:mongo@localhost:27017/?authSource=admin"; 
+// admin:secret are username/password if auth is enabled
+
+// Database and client
+const client = new MongoClient(uri);
+
 
 @Injectable()
 export class DatabaseService {
+    async getDataById(uploadId: string) {
+        await client.connect();
+        const db = client.db("qrcodevalidator"); // choose your DB
+        const QrCodeCollection = db.collection("QrCodes"); // choose your collection
 
+        const results = await QrCodeCollection.find().toArray();
+
+        let validCount = 0;
+        let invalidCount = 0;
+        let unreadableCount = 0;
+        let duplicatesCount = 0;
+
+        for (let index = 0; index < results.length; index++) {
+            if (results[index].status === "VALID")
+                validCount++;
+            else if (results[index].status === "INVALID")
+                invalidCount++;
+            else if (results[index].status === "UNREADABLE")
+                unreadableCount++;
+            else if (results[index].status === "DUPLICATE")
+                duplicatesCount++;
+        }
+        const finalResult = {
+            valid: validCount,
+            invalid: invalidCount,
+            unreadable: unreadableCount,
+            duplicates: duplicatesCount
+        }
+        return finalResult;
+    }
 }
