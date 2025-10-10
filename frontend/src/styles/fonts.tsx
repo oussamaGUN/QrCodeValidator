c2fa7035454ca2f46412c86210dac84691ad10ad
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-10 17:33:59 +0100

    feat: add Orbitron font configuration for styling

--- frontend/src/styles/fonts.tsx
+++ frontend/src/styles/fonts.tsx
@@ -1,6 +1,11 @@
 import { Poppins } from 'next/font/google';
+import { Orbitron } from "next/font/google";
 
 export const poppins = Poppins({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600'],
+});
+export const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "700"], // choose what you actually use
 });
