0f871f2fb247a9e8a5337972058a0b66e70230b3
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-10 23:14:20 +0100

    refactor: comment out image saving logic in pdfToImage function because its only work on host machine

--- backend/src/QrCodeValidator/utils/pdftoimage.ts
+++ backend/src/QrCodeValidator/utils/pdftoimage.ts
@@ -7,11 +7,11 @@ export const pdfToImage = async (buffer: Buffer) => {
         outputFileMaskFunc: (pageNum: number) => `page_${pageNum}`,
     });
     
-    const outputDir = "/home/oussama/Desktop/QrCodeValidator/backend/src/QrCodeValidator/utils/images";
-    images.forEach((image, index) => {
-        const filePath = path.join(outputDir, `page_${index + 1}.png`); // add filename
-        fs.writeFileSync(filePath, image.content); // save the buffer as PNG
-    });
+    // const outputDir = "/home/oussama/Desktop/QrCodeValidator/backend/src/QrCodeValidator/utils/images";
+    // images.forEach((image, index) => {
+    //     const filePath = path.join(outputDir, `page_${index + 1}.png`); // add filename
+    //     fs.writeFileSync(filePath, image.content); // save the buffer as PNG
+    // });
     
     return images;
 };
