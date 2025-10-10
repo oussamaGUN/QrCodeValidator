1c77e45152d6e78ca349d870994c31a397d16222
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-10 17:33:34 +0100

    feat: add Processing component with loading button

---
+++ frontend/src/pages/components/processing.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+function Processing() {
+  return (
+    <div style={{
+    }}>
+        <Button loading sx={{ background: "none"        }}>
+          Submit
+        </Button>
+    </div>
+  )
+}
+
+export default Processing
