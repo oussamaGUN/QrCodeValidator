c92dd9d03f8636117b2e42bcd62e664752608721
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-11 09:07:34 +0100

    remove the button and add new comp HTML don't allow button as a child of another button

--- frontend/src/pages/components/processing.tsx
+++ frontend/src/pages/components/processing.tsx
@@ -1,15 +1,18 @@
-import { Button } from '@mui/material'
-import React from 'react'
+import { Box, CircularProgress } from "@mui/material";
+import React from "react";
 
 function Processing() {
   return (
-    <div style={{
-    }}>
-        <Button loading sx={{ background: "none"        }}>
-          Submit
-        </Button>
+    <div style={{}}>
+      <Box
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        <CircularProgress size={18} sx={{
+          color: "#ef233c"
+        }}/>
+      </Box>
     </div>
-  )
+  );
 }
 
-export default Processing
+export default Processing;
