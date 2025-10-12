8de61b7fc376df55ebbf5534eab54797d0d25306
oussama sabbar <sebbaroussama3@gmail.com>
2025-10-12 19:58:42 +0100

    cleaning

--- backend/src/QrCodeValidator/utils/pdftoimage.ts
+++ backend/src/QrCodeValidator/utils/pdftoimage.ts
@@ -6,7 +6,7 @@ export const pdfToImage = async (buffer: Buffer) => {
     const images = await pdfToPng(buffer, {
         outputFileMaskFunc: (pageNum: number) => `page_${pageNum}`,
     });
-    
+
     // const outputDir = "/home/oussama/Desktop/QrCodeValidator/backend/src/QrCodeValidator/utils/images";
     // images.forEach((image, index) => {
     //     const filePath = path.join(outputDir, `page_${index + 1}.png`); // add filename
